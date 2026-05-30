8b080fd185208eb1ad5eff71117c642007370a3c
Jacob Robinson <Jacobrobinson4321@gmail.com>
2026-05-29 20:26:38 -0400

    Add Transcription View to STT Demo

--- RoleBot/Assets/Demos/STT/Assets/Scripts/STTDemo.cs
+++ RoleBot/Assets/Demos/STT/Assets/Scripts/STTDemo.cs
@@ -10,10 +10,38 @@ namespace RoleBot.STT.Demos
         public TMP_Text transcriptionOutput;
         public Button micToggle;
 
+        private bool micOn = false;
+
         // Start is called once before the first execution of Update after the MonoBehaviour is created
         void Start()
         {
-            
+            sttEngine.onTranscriptionUpdated.AddListener((string output) =>
+            {
+                transcriptionOutput.text = output + "...";
+            });
+
+
+            sttEngine.onTranscriptionCompleted.AddListener((string output) =>
+            {
+                transcriptionOutput.text = output;
+            });
+
+            micToggle.onClick.AddListener(() =>
+            {
+                TMP_Text t = micToggle.gameObject.GetComponentInChildren<TMP_Text>();
+                if (micOn)
+                {
+                    sttEngine.MicOff();
+                    t.text = "START";
+                    micOn = false;
+                }
+                else
+                {
+                    sttEngine.MicOn(); 
+                    t.text = "STOP";
+                    micOn = true;
+                }
+            });
         }
 
         // Update is called once per frame
