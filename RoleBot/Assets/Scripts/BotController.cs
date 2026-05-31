9a5f1561fdab1c4e437248808a637481b4273250
Jacob Robinson <Jacobrobinson4321@gmail.com>
2026-05-31 12:59:42 -0400

    TTS Sentence optimization
    Instead of waiting for the whole chat message to come through at once, send it as chunks divided by punctuation.

--- RoleBot/Assets/Scripts/BotController.cs
+++ RoleBot/Assets/Scripts/BotController.cs
@@ -1,19 +1,26 @@
 // Written by Jacob Robinson, May 2026
 // Last Updated: 5.31.26
 
+using System;
 using RoleBot.Chat;
 using RoleBot.STT;
 using RoleBot.TTS;
+using UnityEditor.Rendering;
 using UnityEngine;
 
 namespace RoleBot
 {
     public class BotController : MonoBehaviour
     {
+        readonly char[] SPLIT_PUNCT = {',', '.', '?', '!'};
+
         [Header("Inference")]
         [SerializeField] private STTEngine stt;
         [SerializeField] private ChatEngine chat;
         [SerializeField] private TTSEngine tts;
+
+        [Header("Role")]
+        [SerializeField] private string voiceID;
 
         // Start is called once before the first execution of Update after the MonoBehaviour is created
         void Start()
@@ -24,7 +31,37 @@ namespace RoleBot
 
         void UserDoneSpeaking(string message)
         {
+            string output = "";
+            int lastPunctuationIndex = 0;
+            var voice = tts.GetVoice(voiceID);
             Debug.Log(message);
+
+            _ = chat.Chat(message, (string s) =>
+            {
+                string newOutput = s.Substring(lastPunctuationIndex);
+                // if (newOutput.Split())
+                output = s;
+                int punct = newOutput.IndexOfAny(SPLIT_PUNCT) + 1;
+                if (punct > 0)
+                {
+                    string sentance = newOutput.Substring(0, punct);
+                    Debug.Log(sentance);
+                    tts.Speak(sentance, voice);
+                    lastPunctuationIndex += punct;
+                }
+                
+                // tts.Speak(newOutput, voice);
+            }, () =>
+            {
+                Debug.Log(output);
+                if (output.Length - lastPunctuationIndex > 1)
+                {
+                    string sentance = output.Substring(lastPunctuationIndex);
+                    Debug.Log(sentance);
+                    tts.Speak(sentance, voice);
+                }
+                // tts.Speak(output, voice);
+            });
         }
 
         // Update is called once per frame
