cb0d4ebac1919c4d92f299a257b280fe9306fc28
Jacob Robinson <Jacobrobinson4321@gmail.com>
2026-06-06 14:28:00 -0400

    Package Structure
    Moved most of the project into a unity package to prepare for exporting.

---
+++ RoleBot/Packages/com.thesevensages.rolebot/Samples/TextToSpeech/Assets/Scripts/TTSSample.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+using RoleBot.TTS.Utils;
+using System.Linq;
+
+namespace RoleBot.TTS.Demos
+{
+    public class TTSSample : MonoBehaviour
+    {
+        public TTSEngine ttsEngine;
+        public TMP_InputField inputField;
+        public TMP_Dropdown voiceChoice;
+        public Button button;
+
+        void Start()
+        {
+            var list = ttsEngine.GetVoicesList();
+            voiceChoice.AddOptions(list.ToList());
+
+            button.onClick.AddListener(TTSSpeak);
+        }
+
+        private void TTSSpeak()
+        {
+            var voice = ttsEngine.GetVoice(voiceChoice.captionText.text);
+            ttsEngine.Speak(inputField.text, voice);
+        }
+
+        void OnDestroy()
+        {
+            button.onClick.RemoveListener(TTSSpeak);            
+        }
+    }
+}
